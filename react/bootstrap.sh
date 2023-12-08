0ddd7cdbe932c1cc37d040b55db5c2c4665f94c5
Jordan Mack <jordan.mack37@gmail.com>
2023-12-08 00:30:28 -0500

    dont track node_modules or env files

---
+++ react/bootstrap.sh
@@ -0,0 +1,11 @@
+# if there is a package.json file
+if [ -f package.json ]; then
+    if [ "$APP_ENV" = "production" ]; then
+            echo "Installing only production node modules"
+            npm install --only=production --omit=dev
+            npm run start
+    else
+            npm install
+            npm run dev
+    fi
+fi
